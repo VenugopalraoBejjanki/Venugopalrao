814b5c607cc000432f42cd503a1592810543f7b4
VenugopalraoBejjanki <venukoti311@gmailcom>
2023-04-03 10:52:28 +0530

    first commit

---
+++ src/main/java/in/ashokit/ReportRestController.java
@@ -0,0 +1,26 @@
+package in.ashokit;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReportRestController {
+	
+	
+	@Autowired
+	private ReportService reportService;
+
+	@GetMapping("/excel")
+	public void generateExcelReport(HttpServletResponse response)throws Exception {
+	
+		response.setContentType("application/octet-stream");
+		
+		String headerkey ="Content-Disposition";
+		String headervalue = "attachment;filename=courses.xls";
+		response.setHeader(headerkey, headervalue);
+		reportService.genralExcel(response);
+	}
+}
